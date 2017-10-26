b49049f28351a798ff15e5e713f87c1541a0c304
Robert Clayton <robbie.c.clayton@gmail.com>
2017-10-26 12:18:30 +0100

    added error if no bikes in dock

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,36 +1,44 @@
 require 'docking_station'
 
 describe DockingStation do
- station = DockingStation.new
    it { is_expected.to respond_to(:release_bike) }
 
-   it 'gets a bike' do
-     expect(station.release_bike).to be_an_instance_of(Bike)
-   end
+  #  it 'gets a bike' do
+  #    expect(station.release_bike).to be_an_instance_of(Bike)
+  #  end
 
    it 'responds to dock' do
+     station = DockingStation.new
      expect(station).to respond_to(:dock)
    end
 
    it 'respond to docked_bikes' do
+     station = DockingStation.new
      expect(station).to respond_to(:docked_bikes)
    end
 
    it 'checks if dock receives one argument' do
+     station = DockingStation.new
      expect(station).to respond_to(:dock).with(1).argument
    end
 
   it "checks if station.docked_bikes is eq to a Bike" do
+    station = DockingStation.new
     test_bike = Bike.new
     station.dock(test_bike)
-    p "here!!!!!", station.docked_bikes
     expect(station.docked_bikes).to eq(test_bike)
   end
 
    it 'checks that station.docked_bikes is an instance_of Bike'do
+    station = DockingStation.new
      bike = Bike.new
      station.dock(bike)
      expect(station.docked_bikes).to be_an_instance_of(Bike)
+   end
+
+   it 'should raise an error if release_bike is requested and docked_bikes is empty' do
+     station = DockingStation.new
+     expect { station.release_bike }.to raise_error("no bikes in dock")
    end
 
 end
